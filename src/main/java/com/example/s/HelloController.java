1b745c039b4cb3b904e85216b9f333502ebfe000
Christopher Albrecht <ch.albrecht33@gmail.com>
2022-01-04 17:12:40 +0100

    ajout des fxml du menu

--- src/main/java/com/example/s/HelloController.java
+++ src/main/java/com/example/s/HelloController.java
@@ -1,14 +1,38 @@
 package com.example.s;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button bouton_historique;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button bouton_jouer;
+
+    @FXML
+    private Button bouton_quitter;
+
+    @FXML
+    private Button bouton_regles;
+
+    @FXML
+    private SplitMenuButton menu_algo;
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+    
 }
